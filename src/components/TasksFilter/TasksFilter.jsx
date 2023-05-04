ed0fe9e28be71745bd9aeae77e777b0dfda2cf04
Maksim Zherdin <maks.kerimov123@gmail.com>
2023-05-04 17:28:46 +0300

    radio

--- src/components/TasksFilter/TasksFilter.jsx
+++ src/components/TasksFilter/TasksFilter.jsx
@@ -3,16 +3,16 @@ import PropTypes from 'prop-types';
 function TasksFilter({ tasksListFiltred }) {
   return (
     <ul className="filters">
-      <li>
-        <input value="all" name="filter" type="radio" className="selected" onClick={() => tasksListFiltred('all')} />
+      <li className="radio">
+        <input id="all" value="all" name="filter" type="radio" defaultChecked onClick={() => tasksListFiltred('all')} />
         <label htmlFor="all">All</label>
       </li>
-      <li>
-        <input value="active" name="filter" type="radio" onClick={() => tasksListFiltred(false)} />
+      <li className="radio">
+        <input id="active" value="active" name="filter" type="radio" onClick={() => tasksListFiltred(false)} />
         <label htmlFor="active">Active</label>
       </li>
-      <li>
-        <input value="completed" name="filter" type="radio" onClick={() => tasksListFiltred(true)} />
+      <li className="radio">
+        <input id="completed" value="completed" name="filter" type="radio" onClick={() => tasksListFiltred(true)} />
         <label htmlFor="completed">Completed</label>
       </li>
     </ul>
